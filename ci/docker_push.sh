db878db8dddf8584eb5e84baad39bda00c7b040f
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2018-07-24 18:43:25 -0500

    Tag as latest as well

--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -1,2 +1,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag odarbelaeze/quotes:$TRAVIS_COMMIT odarbelaeze/quotes:latest
 docker push odarbelaeze/quotes:$TRAVIS_COMMIT
+docker push odarbelaeze/quotes:latest
+
